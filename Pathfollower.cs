fdb983f6a51f4e56b6ac10dfbe8ef9e46a4d526b
Matt <mfarncomb101@gmail.com>
2020-06-29 18:47:11 +1000

    Improve speed of straightening path by a lot

--- Pathfollower.cs
+++ Pathfollower.cs
@@ -20,6 +20,10 @@ public class Pathfollower : MonoBehaviour {
 
     private Stack<Neighbour> normalPath;
     private Stack<Neighbour> straightPath;
+    private Stack<Neighbour> path;
+    private Neighbour originNeighbour;
+    bool pathStarted = false;
+    bool pathComplete = false;
 
     /////////////////////DEBUGGING TEST/////////////////////
     public bool allowedToHunt = false;
@@ -30,6 +34,7 @@ public class Pathfollower : MonoBehaviour {
         gridArr = grid.gridArr;
         gWidth = grid.gridWidth;
         gHeight = grid.gridHeight;
+        
         normalPath = new Stack<Neighbour>();
         straightPath = new Stack<Neighbour>();
         //StartCoroutine(TestRoutine());
@@ -51,21 +56,35 @@ public class Pathfollower : MonoBehaviour {
     
 
     private void Update() { 
-        if (allowedToHunt) {
-           
-         
-            Neighbour n = makePath(createInitialNeighbour());
 
-            //normalPath = getNormalPath(n);
+        if (allowedToHunt) findPath(path);
+ 
+    }
 
-            straightPath = straightenPath(n, n.parent);
+    void findPath(Stack<Neighbour> newPath) {
+
+        newPath = new Stack<Neighbour>();
+        allowedToHunt = false;
+    
+        if (!pathStarted) {
+            pathStarted = true;
+
+            // Create new stopwatch.
+            Stopwatch stopwatch = new Stopwatch();
+            // Begin timing.
+            stopwatch.Start();
             
-            drawPath(straightPath);
-          
-            allowedToHunt = false;
-
+            StartCoroutine(makePath(createInitialNeighbour(), 
+                (normal)=> {
+                    normalPath = normal;
+                    stopwatch.Stop();
+                    UnityEngine.Debug.Log(("Pathfinding: ", stopwatch.ElapsedMilliseconds));
+                    stopwatch.Reset();
+                }
+            ));
 
         }
+ 
     }
 
    Neighbour createInitialNeighbour() {
@@ -74,6 +93,7 @@ public class Pathfollower : MonoBehaviour {
         
         //Vector3Int originIndex = new Vector3Int(gridX, gridY, 0);
         Vector3Int originIndex = _Tools.createVector3Int(gridX, gridY);
+        Neighbour inital = new Neighbour(originIndex, true, target); 
         return new Neighbour(originIndex, true, target);     
    }
 
@@ -133,11 +153,34 @@ public class Pathfollower : MonoBehaviour {
     }
 
     // when moved to next gn (whihc was the first on ordered list) call get neighbours again unless is target
-    private Neighbour makePath(Neighbour n) {
+    IEnumerator makePath(Neighbour n, System.Action<Stack<Neighbour>> OnFinishNormalPath) {
         if (target != new Vector3Int(n.position.x, n.position.y, 0))  {
-            return makePath(getNeighbours(n));
+            yield return null;
+           
+            yield return makePath(getNeighbours(n), OnFinishNormalPath);
         }
-        else return n; 
+        //else return n; 
+        else {
+  
+            OnFinishNormalPath(getNormalPath(n));
+            yield return null;
+
+            // Create new stopwatch.
+            Stopwatch stopwatch2 = new Stopwatch();
+
+            // Begin timing.
+            stopwatch2.Start();
+     
+            StartCoroutine(straighten(getNormalPath(n).ToList(),  new Stack<Neighbour>(),
+                    (straight)=> { 
+                        stopwatch2.Stop();
+                        UnityEngine.Debug.Log(("Straightening: ", stopwatch2.ElapsedMilliseconds));
+                        stopwatch2.Reset();
+                        drawPath(straight);
+                    }
+                ));
+            
+        }
     }
 
     private Stack<Neighbour> getNormalPath(Neighbour n, Stack<Neighbour> path=null) {
@@ -168,49 +211,130 @@ public class Pathfollower : MonoBehaviour {
         return _Tools.createVector3Int(nextX, nextY);
     }
 
+    bool stepTowardsTarget(Neighbour current, Neighbour target, Vector3Int direction) {
+        //UnityEngine.Debug.Log("Target:" + target.position + " Current: " + current.position);
+        //grid.getGridNode(current.position).GetComponent<SpriteRenderer>().color = Color.green;
+        if (current.position == target.position) return true;
+        else if (current.walkable && grid.inBounds(current.position)) {
+            
+            Neighbour newCurrent = getCurrent(current, target, direction);
+            return stepTowardsTarget(newCurrent, target, direction);
+        }  
+        else return false;
+    }
+   
+    IEnumerator straighten(List<Neighbour> path, 
+        Stack<Neighbour> returningPath,
+        System.Action<Stack<Neighbour>> OnComplete)
+         {
+            if (path.Count > 0) {
+                //yield return null;
 
-    Stack<Neighbour> straightenPath(Neighbour origin, 
-        Neighbour target, Vector3Int direction=new Vector3Int(),  
+                //to be the list to iterate through next time round
+                //need s linked list so I can add items to star instead of end
+                LinkedList<Neighbour> newPath= new LinkedList<Neighbour>();
+                foreach (var origin in path) {
+                    int pathSize = path.Count - 1;
+                    for (var i = pathSize; i > 0; i--) {
+                        Neighbour target = path[i];
+                        
+                        //yield return null; - DON'T DELETE - MIGHT NEED IT LATER
+
+                        Vector3Int direction = getDirection(origin, target);
+                        bool canReachTarget;
+                        if (!isWithinCompassDirection(origin.position, target.position)) canReachTarget = false;
+                        else canReachTarget = stepTowardsTarget(origin, target, direction);
+                        
+                        if (canReachTarget) { // has reached target so add that new connection and ignore connctions closer to start
+                            returningPath.Push(target);
+                        
+                            yield return StartCoroutine(straighten(newPath.ToList(), returningPath, OnComplete)); //the stack added will consts of any thing after the target that was actually reached, dropping those before
+                            yield break;
+                        } else { // else can't reach target so check next target
+                            newPath.AddFirst(target); //couldn't get it it so will need to see if other origins can get to it
+                        }; 
+                    }
+                }
+            }
+            else {
+                // exit co routine and return the path
+                OnComplete(returningPath);
+                yield break;
+            }
+    }
+
+
+    IEnumerator straightenPath(Neighbour origin, 
+        Neighbour target, 
+        System.Action<Stack<Neighbour>> OnFinishStraight,
+        Vector3Int direction=new Vector3Int(),  
         Neighbour current=null, Neighbour child=null, 
         Neighbour newChild=null, Stack<Neighbour> path=null, 
-        bool directionNeeded = true) {
+        bool directionNeeded = true
+        ) {
+
+            
+            //UnityEngine.Debug.Log("Starting");
+           
+            yield return null;
             
             // Initialise certain values on first call
             if (path == null) {
                 path = new Stack<Neighbour>();
                 current = origin;
+                //if (!isWithinCompassDirection(current.position, target.position)) initialCheck = true;
             }
 
-            if (target.parent == null) {
+            //UnityEngine.Debug.Log("current :" + current.position);
+            
+
+            if (target == null) {
+                //UnityEngine.Debug.Log("final target is:" +  target.position);
                 path.Push(origin); 
                 directionNeeded = true;
+                //UnityEngine.Debug.Log("coming here");
 
                 // EXIT FUNCTION
-                if (newChild == null ) {
-                    //path.Push(current);
-                    path.Push(target);
-                    path.Push(origin.parent);
-                    return path; 
+                // if (newChild == null ) {
+                    
+                //     //path.Push(current);
+                //     //path.Push(target);
+                //     path.Push(child);
+                //     path.Push(origin.parent);
+                    
+                //     //yield return null;
+                //     drawPath(path);
+                //     yield break;
+                //     //return path; 
                 
-                } else { // CHANGE TO NEXT ORIGIN
-                    return straightenPath(newChild, newChild.parent, 
+                // } 
+                //else { // CHANGE TO NEXT ORIGIN
+                yield return StartCoroutine(straightenPath(newChild, newChild.parent, 
+                        OnFinishStraight,
                         direction, newChild, 
                         null, null, 
-                        path, directionNeeded); 
-                    }           
+                        path, directionNeeded)); 
+                     
+                //    }   
+                yield break;        
             }
+            
             // If direction of path straightening has not been set
             if (directionNeeded) {
                 direction = getDirection(current, target);
                 directionNeeded = false;
             }
+
+
             // If current goes outside of the grid, can't reach target so go to next target
             if (!grid.inBounds(
                 current.position.x + direction.x, current.position.y + direction.y)) {
-                    return straightenPath(origin, target.parent, 
+                    yield return StartCoroutine(straightenPath(origin, target.parent, 
+                        OnFinishStraight,
                         direction, origin, 
                         child, newChild, 
-                        path, true);
+                        path, true));
+                    yield break;
             }
 
             Neighbour newCurrent = getCurrent(current, target, direction);
@@ -219,24 +343,52 @@ public class Pathfollower : MonoBehaviour {
             // and current target is now the child
             //Next target is tha parent's parent
             if (newCurrent.position == target.position) {
-                return straightenPath(origin, target.parent, 
+                if (target.parent == null) {
+                    UnityEngine.Debug.Log("here only once");
+                    path.Push(target);
+                    path.Push(origin);
+                    //drawPath(path);
+                    OnFinishStraight(path);
+                    yield break;
+                    // yield return StartCoroutine(straightenPath(origin, null, 
+                    //     direction, origin, 
+                    //     target, newChild, 
+                    //     path, directionNeeded));
+                }
+                
+                yield return StartCoroutine(straightenPath(origin, target.parent, 
+                    OnFinishStraight,
                     direction, origin, 
                     target, newChild, 
-                    path, directionNeeded);
+                    path, directionNeeded));
+                yield break;
             // if unwalkable, we couldnt get to that target so go back to the start and try to get next target
             // newchild stores the first child. Otherwise later children would replace it and we dont want that
             } else if (!newCurrent.walkable || 
                 !isWithinCompassDirection(newCurrent.position, target.position)) {
                     if (newChild == null) newChild = child;
-                        return straightenPath(origin, target.parent, 
+                    if (target.parent == null) {
+                        yield return StartCoroutine(straightenPath(origin, null, 
+                            OnFinishStraight,
                             direction, origin, 
                             child, newChild, 
-                            path, directionNeeded);
+                            path, directionNeeded));
+                        yield break;
+                    }
+                        yield return StartCoroutine(straightenPath(origin, target.parent,
+                            OnFinishStraight, 
+                            direction, origin, 
+                            child, newChild, 
+                            path, directionNeeded));
+                        yield break;
             } else {   
-                return straightenPath(origin, target, 
+                //UnityEngine.Debug.Log("target is:  " + target.position + " from " + origin.position);
+                yield return StartCoroutine(straightenPath(origin, target,
+                    OnFinishStraight, 
                     direction, newCurrent, 
                     child, newChild, 
-                    path, directionNeeded);
+                    path, directionNeeded));
+                yield break;
             }
     }
 
@@ -258,6 +410,10 @@ public class Pathfollower : MonoBehaviour {
             UnityEngine.Debug.Log(e.position);
         }
         
+    }
+
+    public Neighbour OnComplete(Neighbour n) {
+        return n;
     }
 
   
